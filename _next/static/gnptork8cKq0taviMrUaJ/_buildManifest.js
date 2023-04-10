0fc2441bead2f287169baf74edf1880d33ad3c3d
simenandre <simenandre@users.noreply.github.com>
2023-04-10 08:52:48 +0000

    deploy: 87f8779f3da1e71f36275eab0700798ed4d865a8

---
+++ _next/static/gnptork8cKq0taviMrUaJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/466-4558238e6ae12c67.js","static/chunks/pages/index-77c6844c41a5a5a4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
