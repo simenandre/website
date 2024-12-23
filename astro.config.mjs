f3dae7f8b54a715959027e76b02818ed10ae20f1
Simen A. W. Olsen <hello@simenandre.no>
2024-12-23 15:51:45 +0100

    add articles to my site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://simenandre.no',
-  integrations: [sitemap()]
+  site: "https://simenandre.no",
+  integrations: [sitemap(), mdx()],
+  markdown: {
+    layouts: {
+      default: "./src/article-layout.astro",
+    },
+  },
 });
+
---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+import { defineCollection, z } from "astro:content";
+
+// Define the schema for the blog collection
+const articleCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      author: z.string(),
+      description: z.string().optional(),
+      image: image(),
+      tags: z.array(z.string()).optional(),
+      language: z.enum(["en", "nb-NO"]).default("en"),
+      alternateUrls: z.record(z.string(), z.string()).optional(),
+      syndications: z.array(z.object({
+        publication: z.string(),
+        url: z.string(),
+        publishedAt: z.date(),
+        rights: z.string().optional(),
+      })).optional(),
+    }),
+});
+
+// Export the collections object
+export const collections = {
+  "article": articleCollection,
+};
