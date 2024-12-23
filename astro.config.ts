29d3672bd9bf0811a1ae220aaceb282f61a3706a
Simen A. W. Olsen <hello@simenandre.no>
2024-12-23 15:55:29 +0100

    bump to astro v5

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://simenandre.no",
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap()],
   markdown: {
     layouts: {
       default: "./src/article-layout.astro",
     },
   },
 });
-
